29497745d7d56f3496c50675acb058ceb69c71de
Peter Lind <petl@kea.dk>
2021-02-08 11:49:17 +0100

    added basics

--- basics.js
+++ basics.js
@@ -1,1 +1,15 @@
-console.log("JavaScript is running")
+"use strict";
+
+console.log("We are running!");
+const bool = true;
+const num = 41
+const str = "41";
+const obj = {
+  cats: 2,
+  cars: 1
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+
+const alsoundf = document.querySelector("#doesnotexist");
